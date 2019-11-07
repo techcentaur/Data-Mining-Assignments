164250173451a4ee796ba189f40f1d3df25ba3c5
yash98 <yashmalviya98@gmail.com>
2019-11-07 21:43:27 +0530

    seperate edge node funcs

--- Research-Project/models.py
+++ Research-Project/models.py
@@ -6,12 +6,12 @@ import operator
 import numpy as np
 
 
-
 DROPOUT_RATE = 0.5
-LAYERS_DMS = [2, 4]
+NODES_LAYERS_DMS = [2, 4]
+EDGES_LAYERS_DMS = [2, 4]
 
 
-def node_gru(input_shape, layers_dms=LAYERS_DMS, dropout_rate=DROPOUT_RATE):
+def node_gru(input_shape, layers_dms=NODES_LAYERS_DMS, dropout_rate=DROPOUT_RATE):
     x = Input(input_shape)
     y = x
     for dm in layers_dms:
@@ -26,18 +26,20 @@ def node_gru(input_shape, layers_dms=LAYERS_DMS, dropout_rate=DROPOUT_RATE):
     return model
 
 
-def edge_gru(input_shape, layers_dms=LAYERS_DMS, dropout_rate=DROPOUT_RATE):
+def edge_gru(input_shape, layers_dms=EDGES_LAYERS_DMS, dropout_rate=DROPOUT_RATE):
     x = Input(input_shape)
-    size = functools.reduce(operator.mul, input_shape, 1)
-    y = Reshape((size,), name='predictions')(x)
+    # size = functools.reduce(operator.mul, input_shape, 1)
+    input_shape_list = list(input_shape)
+    y = Reshape(tuple(
+        input_shape_list[:2]+[input_shape_list[-2]*input_shape_list[-1]]), name='predictions')(x)
     for dm in layers_dms:
         y = GRU(dm, activation='tanh', dropout=dropout_rate,
                 recurrent_dropout=dropout_rate, return_sequences=True)(y)
     # size = flattened version of the output shape
-    size = functools.reduce(operator.mul, input_shape, 1)
-    print(size)
-    print(input_shape)
-    y = GRU(size, activation='tanh', dropout=dropout_rate,
+    # size = functools.reduce(operator.mul, input_shape, 1)
+    # print(size)
+    # print(input_shape)
+    y = GRU(input_shape_list[-1]*input_shape_list[-2], activation='tanh', dropout=dropout_rate,
             recurrent_dropout=dropout_rate, return_sequences=True)(y)
     model = Model(inputs=x, outputs=y)
     model.summary()
@@ -51,5 +53,5 @@ def edge_gru(input_shape, layers_dms=LAYERS_DMS, dropout_rate=DROPOUT_RATE):
     return model
 
 
-model = node_gru((10, 20))
+model = edge_gru((10, 20))
 model.fit(np.random.rand(32, 10, 20), np.random.rand(32, 10, 20), batch_size=8)
